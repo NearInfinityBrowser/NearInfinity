37a6c44ee1b96435e2fba668279a3eeacc145f75
Fredrik Lindgren <lindgren.fredrik@gmail.com>
2013-06-21 13:05:24 +0200

    Fix coding style in *CI.java files

--- src/infinity/util/FileOutputStreamCI.java
+++ src/infinity/util/FileOutputStreamCI.java
@@ -3,11 +3,11 @@ package infinity.util;
 import java.io.*;
 
 public class FileOutputStreamCI extends FileOutputStream {
-	public FileOutputStreamCI (String s) throws FileNotFoundException {
-		super(s.toLowerCase());
-	}
-	
-	public FileOutputStreamCI (File f) throws FileNotFoundException {
-		super(f);
-	}
+  public FileOutputStreamCI (String s) throws FileNotFoundException {
+    super(s.toLowerCase());
+  }
+
+  public FileOutputStreamCI (File f) throws FileNotFoundException {
+    super(f);
+  }
 }
