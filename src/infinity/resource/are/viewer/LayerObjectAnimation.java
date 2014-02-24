64649e0612a9e995ba8a26da626c1e3b7b9c27f0
Argent77 <argent77@server.fake>
2014-02-24 21:28:41 +0100

    Area viewer: Added visual enhancements to the animation layer

--- src/infinity/resource/are/viewer/LayerObjectAnimation.java
+++ src/infinity/resource/are/viewer/LayerObjectAnimation.java
@@ -8,11 +8,13 @@ import java.awt.Image;
 import java.awt.Point;
 
 import infinity.datatype.DecNumber;
+import infinity.datatype.Flag;
 import infinity.datatype.TextString;
 import infinity.gui.layeritem.AbstractLayerItem;
 import infinity.gui.layeritem.IconLayerItem;
 import infinity.icon.Icons;
 import infinity.resource.AbstractStruct;
+import infinity.resource.ResourceFactory;
 import infinity.resource.are.Animation;
 import infinity.resource.are.AreResource;
 
@@ -22,8 +24,12 @@ import infinity.resource.are.AreResource;
  */
 public class LayerObjectAnimation extends LayerObject
 {
-  private static final Image[] Icon = new Image[]{Icons.getImage("Animation.png"),
-                                                      Icons.getImage("Animation_s.png")};
+  private static final Image[][] Icon = new Image[][]{
+    {Icons.getImage("Animation.png"), Icons.getImage("Animation_s.png")},
+    {Icons.getImage("AnimationWBM.png"), Icons.getImage("AnimationWBM_s.png")},
+    {Icons.getImage("AnimationPVRZ.png"), Icons.getImage("AnimationPVRZ_s.png")},
+    {Icons.getImage("AnimationBAM.png"), Icons.getImage("AnimationBAM_s.png")}
+  };
   private static Point Center = new Point(16, 17);
 
   private final Animation anim;
@@ -94,18 +100,30 @@ public class LayerObjectAnimation extends LayerObject
   {
     if (anim != null) {
       String msg = "";
+      int iconIdx = 0;
       try {
         location.x = ((DecNumber)anim.getAttribute("Location: X")).getValue();
         location.y = ((DecNumber)anim.getAttribute("Location: Y")).getValue();
+        Flag flags = (Flag)anim.getAttribute("Appearance");
+        if (ResourceFactory.getGameID() == ResourceFactory.ID_BGEE ||
+            ResourceFactory.getGameID() == ResourceFactory.ID_BG2EE) {
+          if (flags.isFlagSet(13)) {
+            iconIdx = 1;
+          } else if (flags.isFlagSet(15)) {
+            iconIdx = 2;
+          } else {
+            iconIdx = 3;
+          }
+        }
         msg = ((TextString)anim.getAttribute("Name")).toString();
       } catch (Exception e) {
         e.printStackTrace();
       }
 
-      item = new IconLayerItem(location, anim, msg, Icon[0], Center);
+      item = new IconLayerItem(location, anim, msg, Icon[iconIdx][0], Center);
       item.setName(getCategory());
       item.setToolTipText(msg);
-      item.setImage(AbstractLayerItem.ItemState.HIGHLIGHTED, Icon[1]);
+      item.setImage(AbstractLayerItem.ItemState.HIGHLIGHTED, Icon[iconIdx][1]);
       item.setVisible(isVisible());
     }
   }
