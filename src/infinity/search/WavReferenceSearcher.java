290dab1b2d39cf889e4dc2188d56faf4d3910f74
Argent77 <argent77@server.fake>
2015-03-06 17:11:03 +0100

    Added missing support for text resources in WAV reference searcher

--- src/infinity/search/WavReferenceSearcher.java
+++ src/infinity/search/WavReferenceSearcher.java
@@ -10,9 +10,12 @@ import infinity.resource.AbstractStruct;
 import infinity.resource.Resource;
 import infinity.resource.StructEntry;
 import infinity.resource.key.ResourceEntry;
+import infinity.resource.text.PlainTextResource;
 import infinity.util.StringResource;
 
 import java.awt.Component;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public final class WavReferenceSearcher extends AbstractReferenceSearcher
 {
@@ -24,7 +27,11 @@ public final class WavReferenceSearcher extends AbstractReferenceSearcher
   @Override
   protected void search(ResourceEntry entry, Resource resource)
   {
-    searchStruct(entry, (AbstractStruct)resource);
+    if (resource instanceof AbstractStruct) {
+      searchStruct(entry, (AbstractStruct)resource);
+    } else if (resource instanceof PlainTextResource) {
+      searchText(entry, (PlainTextResource)resource);
+    }
   }
 
   private void searchStruct(ResourceEntry entry, AbstractStruct struct)
@@ -40,6 +47,20 @@ public final class WavReferenceSearcher extends AbstractReferenceSearcher
         addHit(entry, null, o);
       else if (o instanceof AbstractStruct)
         searchStruct(entry, (AbstractStruct)o);
+    }
+  }
+
+  private void searchText(ResourceEntry entry, PlainTextResource text)
+  {
+    String name = getTargetEntry().getResourceName();
+    int idx = name.lastIndexOf('.');
+    if (idx > 0) {
+      String nameBase = name.substring(0, idx);
+      Pattern p = Pattern.compile("\\b" + nameBase + "\\b", Pattern.CASE_INSENSITIVE);
+      Matcher m = p.matcher(text.getText());
+      if (m.find()) {
+        addHit(entry, null, null);
+      }
     }
   }
 }
