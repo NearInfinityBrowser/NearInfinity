23fbcf3b9c8456d54f3d34b45d84224258dc7e8c
Argent77 <argent77@server.fake>
2015-04-21 21:15:43 +0200

    Expanded Variable structures in GAM resources

--- src/infinity/resource/gam/Variable.java
+++ src/infinity/resource/gam/Variable.java
@@ -6,10 +6,12 @@ package infinity.resource.gam;
 
 import infinity.datatype.Bitmap;
 import infinity.datatype.DecNumber;
+import infinity.datatype.FloatNumber;
 import infinity.datatype.TextString;
 import infinity.datatype.Unknown;
 import infinity.resource.AbstractStruct;
 import infinity.resource.AddRemovable;
+import infinity.resource.StructEntry;
 
 class Variable extends AbstractStruct implements AddRemovable
 {
@@ -44,11 +46,28 @@ class Variable extends AbstractStruct implements AddRemovable
   @Override
   public int read(byte buffer[], int offset) throws Exception
   {
+    // using simplified structure format for GAM V1.1  resources
+    boolean isV1 = true;
+    if (getParent() instanceof AbstractStruct) {
+      StructEntry field = ((AbstractStruct)getParent()).getAttribute(4, false);
+      if (field instanceof TextString) {
+        isV1 = ((TextString)field).toString().equals("V1.1");
+      }
+    }
+
     addField(new TextString(buffer, offset, 32, "Name"));
-    addField(new Bitmap(buffer, offset + 32, 2, "Type", s_type));
-    addField(new Unknown(buffer, offset + 34, 6));
-    addField(new DecNumber(buffer, offset + 40, 4, "Value"));
-    addField(new Unknown(buffer, offset + 44, 40));
+    if (isV1) {
+      addField(new Unknown(buffer, offset + 32, 8));
+      addField(new DecNumber(buffer, offset + 40, 4, "Value"));
+      addField(new Unknown(buffer, offset + 44, 40));
+    } else {
+      addField(new Bitmap(buffer, offset + 32, 2, "Type", s_type));
+      addField(new DecNumber(buffer, offset + 34, 2, "Reference value"));
+      addField(new DecNumber(buffer, offset + 36, 4, "Dword value"));
+      addField(new DecNumber(buffer, offset + 40, 4, "Integer value"));
+      addField(new FloatNumber(buffer, offset + 44, 8, "Double value"));
+      addField(new TextString(buffer, offset + 52, 32, "Script name"));
+    }
     return offset + 84;
   }
 }
