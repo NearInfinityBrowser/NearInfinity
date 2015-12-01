c52e203220c9470c4a7152df4bcbb99007da189e
Argent77 <argent77@server.fake>
2015-12-01 18:44:32 +0100

    Added missing copyright message to a number of source files

--- src/infinity/resource/are/viewer/icon/ViewerIcons.java
+++ src/infinity/resource/are/viewer/icon/ViewerIcons.java
@@ -1,3 +1,7 @@
+// Near Infinity - An Infinity Engine Browser and Editor
+// Copyright (C) 2001 - 2005 Jon Olav Hauglid
+// See LICENSE.txt for license information
+
 package infinity.resource.are.viewer.icon;
 
 import infinity.icon.Icons;
