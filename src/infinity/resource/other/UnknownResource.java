8f2ed8f6ab56ccf066322ede5019922db491cb69
Argent77 <argent77@server.fake>
2016-01-09 23:35:52 +0100

    Added hex edit tab to UnknownResource class

--- src/infinity/resource/other/UnknownResource.java
+++ src/infinity/resource/other/UnknownResource.java
@@ -9,6 +9,7 @@ import infinity.gui.ButtonPanel;
 import infinity.gui.InfinityTextArea;
 import infinity.gui.ViewerUtil;
 import infinity.gui.WindowBlocker;
+import infinity.gui.hexview.GenericHexViewer;
 import infinity.resource.Closeable;
 import infinity.resource.Profile;
 import infinity.resource.Resource;
@@ -20,9 +21,10 @@ import infinity.resource.key.FileResourceEntry;
 import infinity.resource.key.ResourceEntry;
 import infinity.util.io.FileNI;
 import infinity.util.io.FileWriterNI;
+import tv.porst.jhexview.DataChangedEvent;
+import tv.porst.jhexview.IDataChangedListener;
 
 import java.awt.BorderLayout;
-import java.awt.CardLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
@@ -42,25 +44,32 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.SwingWorker;
+import javax.swing.event.CaretEvent;
+import javax.swing.event.CaretListener;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 
 public final class UnknownResource implements Resource, Closeable, Writeable, ActionListener,
-                                              ChangeListener, DocumentListener
+                                              ChangeListener, DocumentListener, CaretListener,
+                                              IDataChangedListener
 {
-  private static final String CARD_BUTTON = "button";
-  private static final String CARD_EDITOR = "editor";
+  private static final int TAB_VIEW = 0;
+  private static final int TAB_TEXT = 1;
+  private static final int TAB_RAW  = 2;
+
+  private static final Charset DEFAULT_CHARSET = Charset.forName("iso-8859-1");
 
   private final ResourceEntry entry;
   private final ButtonPanel buttonPanel = new ButtonPanel();
 
   private JTabbedPane tabbedPane;
   private InfinityTextArea editor;
+  private GenericHexViewer hexViewer;
   private JButton bShowEditor;
-  private JPanel panelMain, panelEdit;
-  private boolean editorActive, textModified;
+  private JPanel panelMain, panelRaw;
+  private boolean textModified, dataSynced;
   private Charset textCharset;
 
   public UnknownResource(ResourceEntry entry) throws Exception
@@ -73,7 +82,7 @@ public final class UnknownResource implements Resource, Closeable, Writeable, Ac
  @Override
  public void close() throws Exception
  {
-   if (isTextModified()) {
+   if (isTextModified() || isRawModified()) {
      File output = null;
      if (entry instanceof BIFFResourceEntry) {
        output = FileNI.getFile(Profile.getRootFolders(),
@@ -104,7 +113,8 @@ public final class UnknownResource implements Resource, Closeable, Writeable, Ac
  public void stateChanged(ChangeEvent e)
  {
    if (e.getSource() == tabbedPane) {
-     setSaveButtonEnabled(isTextModified());
+     synchronizeData(tabbedPane.getSelectedIndex());
+     updateStatusBar();
    }
  }
 
@@ -124,6 +134,7 @@ public final class UnknownResource implements Resource, Closeable, Writeable, Ac
     else if (event.getSource() == buttonPanel.getControlByType(ButtonPanel.Control.Save)) {
       if (ResourceFactory.saveResource(this, panelMain.getTopLevelAncestor())) {
         setTextModified(false);
+        setRawModified(false);
       }
     }
   }
@@ -135,26 +146,44 @@ public final class UnknownResource implements Resource, Closeable, Writeable, Ac
   @Override
   public void insertUpdate(DocumentEvent e)
   {
-    textModified = true;
-    setSaveButtonEnabled(isTextModified());
+    setTextModified(true);
   }
 
   @Override
   public void removeUpdate(DocumentEvent e)
   {
-    textModified = true;
-    setSaveButtonEnabled(isTextModified());
+    setTextModified(true);
   }
 
   @Override
   public void changedUpdate(DocumentEvent e)
   {
-    textModified = true;
-    setSaveButtonEnabled(isTextModified());
+    setTextModified(true);
   }
 
 // --------------------- End Interface DocumentListener ---------------------
 
+// --------------------- Begin Interface CaretListener ---------------------
+
+  @Override
+  public void caretUpdate(CaretEvent e)
+  {
+    if (e.getSource() == editor) {
+      updateStatusBar();
+    }
+  }
+
+// --------------------- End Interface CaretListener ---------------------
+
+// --------------------- Begin Interface IDataChangedListener ---------------------
+
+  @Override
+  public void dataChanged(DataChangedEvent event)
+  {
+    setRawModified(true);
+  }
+
+// --------------------- End Interface IDataChangedListener ---------------------
 
 // --------------------- Begin Interface Resource ---------------------
 
@@ -176,32 +205,40 @@ public final class UnknownResource implements Resource, Closeable, Writeable, Ac
     ((JButton)buttonPanel.addControl(ButtonPanel.Control.Save)).addActionListener(this);
     buttonPanel.getControlByType(ButtonPanel.Control.Save).setEnabled(false);
 
-    JPanel panelView = new JPanel(new BorderLayout());
-    JLabel label = new JLabel("Unsupported file format", JLabel.CENTER);
-    panelView.add(label, BorderLayout.CENTER);
-    panelView.add(buttonPanel, BorderLayout.SOUTH);
-    label.setBorder(BorderFactory.createLoweredBevelBorder());
-
-    CardLayout cl = new CardLayout();
-    panelEdit = new JPanel(cl);
-
-    JPanel panelEditButton = new JPanel(new GridBagLayout());
     GridBagConstraints gbc = new GridBagConstraints();
-    gbc = ViewerUtil.setGBC(gbc, 0, 0, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER,
-                            GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0);
+
+    // creating View tab
+    JPanel panelView = new JPanel(new GridBagLayout());
+    panelView.setBorder(BorderFactory.createLoweredBevelBorder());
+    JLabel label = new JLabel("Unsupported file format", JLabel.CENTER);
     bShowEditor = new JButton("Edit as text");
     bShowEditor.addActionListener(this);
-    panelEditButton.add(bShowEditor, gbc);
-    panelEdit.add(panelEditButton, CARD_BUTTON);
+    gbc = ViewerUtil.setGBC(gbc, 0, 0, 1, 1, 1.0, 1.0, GridBagConstraints.CENTER,
+                            GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0);
+    panelView.add(new JLabel(), gbc);
+    gbc = ViewerUtil.setGBC(gbc, 0, 1, 1, 1, 1.0, 0.0, GridBagConstraints.CENTER,
+                            GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0);
+    panelView.add(label, gbc);
+    gbc = ViewerUtil.setGBC(gbc, 0, 2, 1, 1, 1.0, 0.0, GridBagConstraints.CENTER,
+                            GridBagConstraints.NONE, new Insets(8, 0, 0, 0), 0, 0);
+    panelView.add(bShowEditor, gbc);
+    gbc = ViewerUtil.setGBC(gbc, 0, 3, 1, 1, 1.0, 1.0, GridBagConstraints.CENTER,
+                            GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0);
+    panelView.add(new JLabel(), gbc);
 
+    // creating (empty) Edit tab
     editor = new InfinityTextArea(true);
     editor.getDocument().addDocumentListener(this);
-    panelEdit.add(new JScrollPane(editor), CARD_EDITOR);
-    cl.show(panelEdit, "BUTTON");
+    editor.addCaretListener(this);
+
+    // creating Raw tab (stub)
+    panelRaw = new JPanel(new BorderLayout());
 
     tabbedPane = new JTabbedPane();
     tabbedPane.addTab("View", panelView);
-    tabbedPane.addTab("Edit", panelEdit);
+    tabbedPane.addTab("Edit", new JScrollPane(editor));
+    tabbedPane.addTab("Raw", panelRaw);
+    tabbedPane.setEnabledAt(TAB_TEXT, false);
     tabbedPane.addChangeListener(this);
 
     panelMain = new JPanel(new BorderLayout());
@@ -218,8 +255,10 @@ public final class UnknownResource implements Resource, Closeable, Writeable, Ac
   @Override
   public void write(OutputStream os) throws IOException
   {
-    if (isTextModified()) {
+    if (tabbedPane.getSelectedIndex() == TAB_TEXT) {
       FileWriterNI.writeString(os, editor.getText(), editor.getText().length(), textCharset);
+    } else {
+      FileWriterNI.writeBytes(os, hexViewer.getData());
     }
   }
 
@@ -227,12 +266,22 @@ public final class UnknownResource implements Resource, Closeable, Writeable, Ac
 
   private boolean isEditorActive()
   {
-    return editorActive;
+    return tabbedPane.isEnabledAt(TAB_TEXT);
   }
 
   private void setEditorActive(boolean activate)
   {
-    editorActive = activate;
+    if (tabbedPane.isEnabledAt(TAB_TEXT) != activate) {
+      if (!activate && tabbedPane.getSelectedIndex() == TAB_TEXT) {
+        tabbedPane.setSelectedIndex(TAB_VIEW);
+      }
+      tabbedPane.setEnabledAt(TAB_TEXT, activate);
+    }
+  }
+
+  private boolean isRawActive()
+  {
+    return (hexViewer != null);
   }
 
   private boolean isTextModified()
@@ -242,9 +291,84 @@ public final class UnknownResource implements Resource, Closeable, Writeable, Ac
 
   private void setTextModified(boolean modified)
   {
-    if (modified != textModified) {
+    if (isEditorActive()) {
       textModified = modified;
+      setDataInSync(!modified);
       setSaveButtonEnabled(isTextModified());
+    }
+  }
+
+  private boolean isRawModified()
+  {
+    return (isRawActive() && hexViewer.isModified());
+  }
+
+  private void setRawModified(boolean modified)
+  {
+    if (isRawActive()) {
+      if (!modified) {
+        hexViewer.clearModified();
+      }
+      setDataInSync(!modified);
+      setSaveButtonEnabled(isRawModified());
+    }
+  }
+
+  // Returns true if content in one of the editors has been modified by the user
+  private boolean isDataInSync()
+  {
+    if (isEditorActive() && isRawActive()) {
+      return dataSynced;
+    } else {
+      return true;
+    }
+  }
+
+  // Mark data as (not) synchronized
+  private void setDataInSync(boolean b)
+  {
+    dataSynced = b;
+  }
+
+  // Synchronizes data in text and raw tabs. Sync target is specified by the tabIndex parameter.
+  private void synchronizeData(int tabIndex)
+  {
+    switch (tabIndex) {
+      case TAB_TEXT:
+      {
+        if (!isDataInSync()) {
+          int pos = editor.getCaretPosition();
+          editor.setText(hexViewer.getText(textCharset));
+          editor.setCaretPosition(Math.min(editor.getDocument().getLength(), pos));
+          editor.discardAllEdits();
+          setDataInSync(true);
+        }
+        editor.requestFocusInWindow();
+        break;
+      }
+      case TAB_RAW:
+      {
+        // lazy initialization
+        if (!isRawActive()) {
+          try {
+            WindowBlocker.blockWindow(true);
+            hexViewer = new GenericHexViewer(entry);
+            hexViewer.addDataChangedListener(this);
+            hexViewer.setCurrentOffset(0L);
+            panelRaw.add(hexViewer, BorderLayout.CENTER);
+          } catch (Exception e) {
+            e.printStackTrace();
+          } finally {
+            WindowBlocker.blockWindow(false);
+          }
+        }
+        if (!isDataInSync()) {
+          hexViewer.setText(editor.getText(), textCharset);
+          setDataInSync(true);
+        }
+        hexViewer.requestFocusInWindow();
+        break;
+      }
     }
   }
 
@@ -253,10 +377,24 @@ public final class UnknownResource implements Resource, Closeable, Writeable, Ac
     buttonPanel.getControlByType(ButtonPanel.Control.Save).setEnabled(enable);
   }
 
+  private void updateStatusBar()
+  {
+    if (isEditorActive() && tabbedPane.getSelectedIndex() == TAB_TEXT) {
+      int row = editor.getCaretLineNumber() + 1;
+      int col = editor.getCaretOffsetFromLineStart() + 1;
+      NearInfinity.getInstance().getStatusBar().setCursorText(Integer.toString(row) + ":" + Integer.toString(col));
+    } else if (isRawActive() && tabbedPane.getSelectedIndex() == TAB_RAW) {
+      hexViewer.updateStatusBar();
+    } else {
+      NearInfinity.getInstance().getStatusBar().setCursorText("");
+    }
+  }
+
   // Opens resource in text editor, optionally ask for confirmation if file is big
   private void openTextEditor(boolean confirmSize)
   {
     if (isEditorActive()) {
+      tabbedPane.setSelectedIndex(TAB_TEXT);
       return;
     }
 
@@ -303,7 +441,7 @@ public final class UnknownResource implements Resource, Closeable, Writeable, Ac
             }
             if (textCharset == null) {
               // fall back to ANSI charset
-              textCharset = Charset.forName("iso-8859-1");
+              textCharset = DEFAULT_CHARSET;
             }
             editor.setText(new String(data, textCharset));
             editor.setCaretPosition(0);
@@ -313,12 +451,10 @@ public final class UnknownResource implements Resource, Closeable, Writeable, Ac
             e.printStackTrace();
           }
           if (success) {
-            CardLayout cl = (CardLayout)panelEdit.getLayout();
-            cl.show(panelEdit, CARD_EDITOR);
-            editor.requestFocusInWindow();
             setEditorActive(true);
             setTextModified(false);
-            setSaveButtonEnabled(isTextModified());
+            tabbedPane.setSelectedIndex(TAB_TEXT);
+            editor.requestFocusInWindow();
           } else {
             JOptionPane.showMessageDialog(NearInfinity.getInstance(),
                                           "Error reading file data.",
