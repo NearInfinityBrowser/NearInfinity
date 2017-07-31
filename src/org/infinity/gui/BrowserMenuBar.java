825a12b75a2d22f5a664801706760be5ceacd56f
Argent77 <argent77@server.fake>
2017-07-31 12:38:01 +0200

    Version v2.1-20170731

--- src/org/infinity/gui/BrowserMenuBar.java
+++ src/org/infinity/gui/BrowserMenuBar.java
@@ -85,7 +85,7 @@ import org.infinity.util.io.FileManager;
 
 public final class BrowserMenuBar extends JMenuBar
 {
-  public static final String VERSION = "v2.1-20170529";
+  public static final String VERSION = "v2.1-20170731";
   public static final int OVERRIDE_IN_THREE = 0, OVERRIDE_IN_OVERRIDE = 1, OVERRIDE_SPLIT = 2;
   public static final LookAndFeelInfo DEFAULT_LOOKFEEL =
       new LookAndFeelInfo("Metal", "javax.swing.plaf.metal.MetalLookAndFeel");
