a691cfec13ae8836ca78ab9caa7958a700d6e351
Argent77 <4519923+Argent77@users.noreply.github.com>
2018-10-31 15:09:02 +0100

    Add convenience entry to EFF V2 source item slots list

--- src/org/infinity/resource/Effect2.java
+++ src/org/infinity/resource/Effect2.java
@@ -19,6 +19,7 @@ import org.infinity.datatype.ResourceRef;
 import org.infinity.datatype.SecTypeBitmap;
 import org.infinity.datatype.TextString;
 import org.infinity.datatype.Unknown;
+import org.infinity.util.IdsMapEntry;
 import org.infinity.util.io.StreamUtils;
 
 public final class Effect2 extends AbstractStruct implements AddRemovable
@@ -104,7 +105,9 @@ public final class Effect2 extends AbstractStruct implements AddRemovable
     } else {
       list.add(new DecNumber(buffer, offset + 84, 4, EFFECT_IMPACT_PROJECTILE));
     }
-    list.add(new IdsBitmap(buffer, offset + 88, 4, EFFECT_SOURCE_ITEM_SLOT, "SLOTS.IDS"));
+    IdsBitmap slot_type = new IdsBitmap(buffer, offset + 88, 4, EFFECT_SOURCE_ITEM_SLOT, "SLOTS.IDS");
+    slot_type.addIdsMapEntry(new IdsMapEntry(4294967295L, "NONE"));
+    list.add(slot_type);
     list.add(new TextString(buffer, offset + 92, 32, EFFECT_VARIABLE_NAME));
     list.add(new DecNumber(buffer, offset + 124, 4, EFFECT_CASTER_LEVEL));
     list.add(new Flag(buffer, offset + 128, 4, EFFECT_INTERNAL_FLAGS, null));
