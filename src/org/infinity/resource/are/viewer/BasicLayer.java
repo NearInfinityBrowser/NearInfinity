d4eaea8d0be7e8818faa0bb592b17908cbf08cba
Mingun <Alexander_Sergey@mail.ru>
2019-11-17 02:44:13 +0500

    AreViewer: Remove unused methods for get layer objects

--- src/org/infinity/resource/are/viewer/BasicLayer.java
+++ src/org/infinity/resource/are/viewer/BasicLayer.java
@@ -98,20 +98,6 @@ public abstract class BasicLayer<E extends LayerObject, R extends AbstractStruct
   }
 
   /**
-   * Returns the layer object at the specified index.
-   * @param index The index of the layer object.
-   * @return The layer object, of {@code null} if not available.
-   */
-  public E getLayerObject(int index)
-  {
-    if (index >= 0 && index < listObjects.size()) {
-      return listObjects.get(index);
-    } else {
-      return null;
-    }
-  }
-
-  /**
    * Returns the list of layer objects for direct manipulation.
    * @return List of layer objects. Never {@code null}
    */
@@ -142,24 +128,6 @@ public abstract class BasicLayer<E extends LayerObject, R extends AbstractStruct
       }
     }
     this.visible = visible;
-  }
-
-  /**
-   * Returns the layer object containing the specified layer item.
-   * @return The object, if it has been found, {@code null} otherwise.
-   */
-  public E getLayerObjectOf(AbstractLayerItem item)
-  {
-    if (item != null) {
-      for (final E obj : listObjects) {
-        for (final AbstractLayerItem layerItem : obj.getLayerItems()) {
-          if (layerItem == item) {
-            return obj;
-          }
-        }
-      }
-    }
-    return null;
   }
 
   /**
@@ -276,12 +244,10 @@ public abstract class BasicLayer<E extends LayerObject, R extends AbstractStruct
    */
   public boolean isScheduled(int index)
   {
-    E obj = getLayerObject(index);
-    if (obj != null) {
-      return !isScheduleEnabled() || obj.isScheduled(getSchedule());
-    } else {
-      return false;
+    if (index >= 0 && index < listObjects.size()) {
+      return !isScheduleEnabled() || listObjects.get(index).isScheduled(getSchedule());
     }
+    return false;
   }
 
   /**
