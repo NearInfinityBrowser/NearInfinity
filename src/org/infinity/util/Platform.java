0cc93d202dce5768e3997e269cffc9e24e0f8c1a
Argent77 <4519923+Argent77@users.noreply.github.com>
2020-06-18 16:16:36 +0200

    Fix display issues in BAM Converter palette dialogs
    Added workaround for a Java bug on Unix systems.

---
+++ src/org/infinity/util/Platform.java
@@ -0,0 +1,49 @@
+// Near Infinity - An Infinity Engine Browser and Editor
+// Copyright (C) 2001 - 2020 Jon Olav Hauglid
+// See LICENSE.txt for license information
+
+package org.infinity.util;
+
+/**
+ * This class can be used to determine the  operating system where this Java application is running.
+ * Determines
+ */
+public class Platform
+{
+  public enum OS {
+    Windows,
+    Unix,
+    MacOS,
+    Solaris,
+    Unknown,
+  }
+
+  /** Whether this a Microsoft Windows system. */
+  public final static boolean IS_WINDOWS  = (getPlatform() == OS.Windows);
+  /** Whether this GNU/Linux or another Unix-like operating system. */
+  public final static boolean IS_UNIX     = (getPlatform() == OS.Unix);
+  /** Whether this an Apple Mac OS X system. */
+  public final static boolean IS_MACOS    = (getPlatform() == OS.MacOS);
+  /** Whether this a Sun OS or Solaris system. */
+  public final static boolean IS_SOLARIS  = (getPlatform() == OS.Solaris);
+
+  /**
+   * Determines the current operating system.
+   */
+  public static OS getPlatform() {
+    String osName = System.getProperty("os.name");
+    if (osName.contains("mac") || osName.contains("darwin")) {
+      return OS.MacOS;
+    } else if (osName.contains("nix") || osName.contains("nux") || osName.contains("aix")) {
+      return OS.Unix;
+    } else if (osName.contains("win")) {
+      return OS.Windows;
+    } else if (osName.contains("sunos") || osName.contains("solaris")) {
+      return OS.Solaris;
+    } else {
+      return OS.Unknown;
+    }
+  }
+
+  private Platform() { }
+}
