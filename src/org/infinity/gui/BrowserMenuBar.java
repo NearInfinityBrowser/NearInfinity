1deddde57ffb7456c59d98c2a31fb049f2c5b16f
Argent77 <4519923+Argent77@users.noreply.github.com>
2022-04-08 11:19:06 +0200

    Version 2.2-20220408

--- src/org/infinity/gui/BrowserMenuBar.java
+++ src/org/infinity/gui/BrowserMenuBar.java
@@ -112,7 +112,7 @@ import org.infinity.util.io.FileManager;
 import org.infinity.util.tuples.Couple;
 
 public final class BrowserMenuBar extends JMenuBar implements KeyEventDispatcher {
-  public static final String VERSION = "v2.2-20211218";
+  public static final String VERSION = "v2.2-20220408";
 
   public static final LookAndFeelInfo DEFAULT_LOOKFEEL =
       new LookAndFeelInfo("Metal", "javax.swing.plaf.metal.MetalLookAndFeel");
