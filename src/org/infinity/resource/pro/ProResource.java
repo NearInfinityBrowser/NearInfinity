NearInfinityBrowser/NearInfinity

commit a85dc4f4b0e7924fe8c5760d604a72be0c62ab6e

ARE Actor, GAM Party member and PRO resource has no elements that can be dynamically added/removed
Also, because getAddRemovables() always return non-null array, remove corresponding checks.
Also remove pointless call of getAddRemovables() - it has no side-effects

src/org/infinity/resource/pro/ProResource.java
@@ -51,7 +51,7 @@ import org.infinity.util.LongIntegerHashMap;
  * @see <a href="https://gibberlings3.github.io/iesdp/file_formats/ie_formats/pro_v1.htm">
  * https://gibberlings3.github.io/iesdp/file_formats/ie_formats/pro_v1.htm</a>
  */
-public final class ProResource extends AbstractStruct implements Resource, HasAddRemovable, HasViewerTabs, UpdateListener
+public final class ProResource extends AbstractStruct implements Resource, HasViewerTabs, UpdateListener
 {
   // PRO-specific field labels
   public static final String PRO_TYPE = "Projectile type";
@@ -130,26 +130,6 @@ public final class ProResource extends AbstractStruct implements Resource, HasAd
   {
     super(entry);
   }
-
-  //<editor-fold defaultstate="collapsed" desc="HasAddRemovable">
-  @Override
-  public AddRemovable[] getAddRemovables() throws Exception
-  {
-    return null;
-  }
-
-  @Override
-  public AddRemovable confirmAddEntry(AddRemovable entry) throws Exception
-  {
-    return entry;
-  }
-
-  @Override
-  public boolean confirmRemoveEntry(AddRemovable entry) throws Exception
-  {
-    return true;
-  }
-  //</editor-fold>
 
   //<editor-fold defaultstate="collapsed" desc="UpdateListener">
   @Override

src/org/infinity/gui/StructViewer.java
@@ -1,5 +1,5 @@
 // Near Infinity - An Infinity Engine Browser and Editor
-// Copyright (C) 2001 - 2019 Jon Olav Hauglid
+// Copyright (C) 2001 - 2020 Jon Olav Hauglid
 // See LICENSE.txt for license information
 
 package org.infinity.gui;
@@ -361,8 +361,6 @@ public final class StructViewer extends JPanel implements ListSelectionListener,
       } catch (Exception e) {
         e.printStackTrace();
       }
-      if (emptyTypes == null)
-        emptyTypes = new AddRemovable[0];
       JMenuItem menuItems[] = new JMenuItem[emptyTypes.length];
       for (int i = 0; i < menuItems.length; i++) {
         menuItems[i] = new JMenuItem(emptyTypes[i].getName());

src/org/infinity/resource/HasAddRemovable.java
@@ -1,5 +1,5 @@
 // Near Infinity - An Infinity Engine Browser and Editor
-// Copyright (C) 2001 - 2005 Jon Olav Hauglid
+// Copyright (C) 2001 - 2020 Jon Olav Hauglid
 // See LICENSE.txt for license information
 
 package org.infinity.resource;
@@ -8,7 +8,8 @@ public interface HasAddRemovable
 {
   /**
    * Returns an array of available {@link AddRemovable} prototype objects.
-   * @return An array of available {@link AddRemovable} objects.
+   *
+   * @return An array of available {@link AddRemovable} objects. Can't be {@code null}
    */
   AddRemovable[] getAddRemovables() throws Exception;
 
@@ -31,4 +32,3 @@ public interface HasAddRemovable
    */
   boolean confirmRemoveEntry(AddRemovable entry) throws Exception;
 }
-

src/org/infinity/resource/are/Actor.java
@@ -19,14 +19,13 @@ import org.infinity.datatype.Unknown;
 import org.infinity.gui.StructViewer;
 import org.infinity.resource.AbstractStruct;
 import org.infinity.resource.AddRemovable;
-import org.infinity.resource.HasAddRemovable;
 import org.infinity.resource.HasViewerTabs;
 import org.infinity.resource.Profile;
 import org.infinity.resource.StructEntry;
 import org.infinity.resource.cre.CreResource;
 import org.infinity.util.io.StreamUtils;
 
-public final class Actor extends AbstractStruct implements AddRemovable, HasViewerTabs, HasAddRemovable
+public final class Actor extends AbstractStruct implements AddRemovable, HasViewerTabs
 {
   // ARE/Actor-specific field labels
   public static final String ARE_ACTOR                      = "Actor";
@@ -90,30 +89,6 @@ public final class Actor extends AbstractStruct implements AddRemovable, HasView
   }
 
 //--------------------- End Interface AddRemovable ---------------------
-
-
-// --------------------- Begin Interface HasAddRemovable ---------------------
-
-  @Override
-  public AddRemovable[] getAddRemovables()
-  {
-    return new AddRemovable[]{};
-  }
-
-  @Override
-  public AddRemovable confirmAddEntry(AddRemovable entry) throws Exception
-  {
-    return entry;
-  }
-
-  @Override
-  public boolean confirmRemoveEntry(AddRemovable entry) throws Exception
-  {
-    return true;
-  }
-
-// --------------------- End Interface HasAddRemovable ---------------------
-
 
 // --------------------- Begin Interface HasViewerTabs ---------------------
 

src/org/infinity/resource/gam/PartyNPC.java
@@ -22,7 +22,6 @@ import org.infinity.datatype.UnsignDecNumber;
 import org.infinity.gui.StructViewer;
 import org.infinity.resource.AbstractStruct;
 import org.infinity.resource.AddRemovable;
-import org.infinity.resource.HasAddRemovable;
 import org.infinity.resource.HasViewerTabs;
 import org.infinity.resource.Profile;
 import org.infinity.resource.StructEntry;
@@ -30,7 +29,7 @@ import org.infinity.resource.cre.CreResource;
 import org.infinity.util.LongIntegerHashMap;
 import org.infinity.util.io.StreamUtils;
 
-public class PartyNPC extends AbstractStruct implements HasViewerTabs, HasAddRemovable, AddRemovable
+public class PartyNPC extends AbstractStruct implements HasViewerTabs, AddRemovable
 {
   // GAM/PartyNPC-specific field labels
   public static final String GAM_NPC                            = "Party member";
@@ -119,29 +118,6 @@ public class PartyNPC extends AbstractStruct implements HasViewerTabs, HasAddRem
   {
     super(superStruct, name, buffer, offset);
   }
-
-// --------------------- Begin Interface HasAddRemovable ---------------------
-
-  @Override
-  public AddRemovable[] getAddRemovables() throws Exception
-  {
-    return new AddRemovable[]{};
-  }
-
-  @Override
-  public AddRemovable confirmAddEntry(AddRemovable struct) throws Exception
-  {
-    return struct;
-  }
-
-  @Override
-  public boolean confirmRemoveEntry(AddRemovable entry) throws Exception
-  {
-    return true;
-  }
-
-// --------------------- End Interface HasAddRemovable ---------------------
-
 
 //--------------------- Begin Interface AddRemovable ---------------------
 

src/org/infinity/util/StructClipboard.java
@@ -130,8 +130,6 @@ public final class StructClipboard
       } catch (Exception e) {
         return CLIPBOARD_EMPTY;
       }
-      if (targetClasses == null)
-        targetClasses = new AddRemovable[0];
       for (final StructEntry entry: contents) {
         if (entry instanceof AddRemovable) {
           if (canConvertToEffectV1(struct, (AddRemovable)entry) ||
@@ -176,7 +174,6 @@ public final class StructClipboard
           index += i;
         }
         if (pasteEntry instanceof HasAddRemovable) {
-          ((HasAddRemovable)pasteEntry).getAddRemovables();
           List<AddRemovable> substructures = ((AbstractStruct)pasteEntry).removeAllRemoveables();
           lastIndex = targetStruct.addDatatype(pasteEntry, index);
           pasteSubStructures((AbstractStruct)pasteEntry, substructures);

commit 6a3e649abc650c81e3717a2115729cc00f7978d0

Fix PRO resources dynamically change size depending on projectile type
Fixes a regression from a85dc4f4b0e7924fe8c5760d604a72be0c62ab6e

src/org/infinity/resource/pro/ProResource.java
@@ -52,7 +52,7 @@ import org.infinity.search.SearchOptions;
  * @see <a href="https://gibberlings3.github.io/iesdp/file_formats/ie_formats/pro_v1.htm">
  *      https://gibberlings3.github.io/iesdp/file_formats/ie_formats/pro_v1.htm</a>
  */
-public final class ProResource extends AbstractStruct implements Resource, HasViewerTabs, UpdateListener {
+public final class ProResource extends AbstractStruct implements Resource, HasChildStructs, HasViewerTabs, UpdateListener {
   // PRO-specific field labels
   public static final String PRO_TYPE                 = "Projectile type";
   public static final String PRO_SPEED                = "Speed";
@@ -193,6 +193,16 @@ public final class ProResource extends AbstractStruct implements Resource, HasVi
       }
     }
     return false;
+  }
+
+  @Override
+  public AddRemovable[] getPrototypes() throws Exception {
+    return new AddRemovable[] {};
+  }
+
+  @Override
+  public AddRemovable confirmAddEntry(AddRemovable entry) throws Exception {
+    return entry;
   }
 
   @Override