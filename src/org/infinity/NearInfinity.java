b1646fa76ce4422c5eff523eee5dbfac065cad6c
Argent77 <4519923+Argent77@users.noreply.github.com>
2023-07-29 14:51:58 +0200

    Version 2.4-20230729

--- src/org/infinity/NearInfinity.java
+++ src/org/infinity/NearInfinity.java
@@ -136,7 +136,7 @@ import org.infinity.util.tuples.Couple;
 
 public final class NearInfinity extends JFrame implements ActionListener, ViewableContainer {
   // the current Near Infinity version
-  private static final String VERSION = "v2.4-20230714";
+  private static final String VERSION = "v2.4-20230729";
 
   // the minimum supported Java version
   private static final int JAVA_VERSION_MIN = 8;
