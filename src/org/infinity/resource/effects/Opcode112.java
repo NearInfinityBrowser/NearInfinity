NearInfinityBrowser/NearInfinity

commit 3fbcc7419994870ae1e5a74348c1acd605d50b6b

Update opcode 112
Added parameter1 for EE games.

src/org/infinity/resource/effects/Opcode112.java
@@ -7,6 +7,7 @@ package org.infinity.resource.effects;
 import java.nio.ByteBuffer;
 import java.util.List;
 
+import org.infinity.datatype.Bitmap;
 import org.infinity.datatype.Datatype;
 import org.infinity.datatype.DecNumber;
 import org.infinity.resource.AbstractStruct;
@@ -16,7 +17,12 @@ import org.infinity.resource.StructEntry;
  * Implementation of opcode 112.
  */
 public class Opcode112 extends BaseOpcode {
+  private static final String EFFECT_SOUND_FX = "Sound effect";
+
   private static final String RES_TYPE = "ITM";
+
+  private static final String[] SOUND_FX_EE = { "Play EFF_M02.WAV", "Play AMB_D02B.WAV", "Play \"Resource 2\"",
+      "No sound" };
 
   /** Returns the opcode name for the current game variant. */
   private static String getOpcodeName() {
@@ -31,6 +37,14 @@ public class Opcode112 extends BaseOpcode {
   protected String makeEffectParamsGeneric(Datatype parent, ByteBuffer buffer, int offset, List<StructEntry> list,
       boolean isVersion1) {
     list.add(new DecNumber(buffer, offset, 4, AbstractStruct.COMMON_UNUSED));
+    list.add(new DecNumber(buffer, offset + 4, 4, AbstractStruct.COMMON_UNUSED));
+    return RES_TYPE;
+  }
+
+  @Override
+  protected String makeEffectParamsEE(Datatype parent, ByteBuffer buffer, int offset, List<StructEntry> list,
+      boolean isVersion1) {
+    list.add(new Bitmap(buffer, offset + 4, 4, EFFECT_SOUND_FX, SOUND_FX_EE));
     list.add(new DecNumber(buffer, offset + 4, 4, AbstractStruct.COMMON_UNUSED));
     return RES_TYPE;
   }

commit 84ad19a7bd0f044ec2b1c4cca206eea2ce24b9ce

Fix a regression from 3fbcc74

src/org/infinity/resource/effects/Opcode112.java
@@ -44,7 +44,7 @@ public class Opcode112 extends BaseOpcode {
   @Override
   protected String makeEffectParamsEE(Datatype parent, ByteBuffer buffer, int offset, List<StructEntry> list,
       boolean isVersion1) {
-    list.add(new Bitmap(buffer, offset + 4, 4, EFFECT_SOUND_FX, SOUND_FX_EE));
+    list.add(new Bitmap(buffer, offset, 4, EFFECT_SOUND_FX, SOUND_FX_EE));
     list.add(new DecNumber(buffer, offset + 4, 4, AbstractStruct.COMMON_UNUSED));
     return RES_TYPE;
   }